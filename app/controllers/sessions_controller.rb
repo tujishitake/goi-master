a6b8538b7a52825022d7b91dc00593efa89e3677
tujishitake <tujishitake@gmail.com>
2017-08-20 10:02:02 +0000

    error-message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       flash[:success] = 'ログインに成功しました。'
       redirect_to @user
     else
-      flash.now[:danger] = 'ログインに失敗しました。'
+      flash.now[:danger] = 'メールアドレスまたはパスワードが間違っています。'
       render :new
     end
   end
