e00adb7b3fcef5d5ca9bcb1da55e11397f2d6123
tujishitake <tujishitake@gmail.com>
2017-10-05 02:32:06 +0000

    image

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -5,7 +5,18 @@ class Flashcard < ApplicationRecord
   validates :text1, presence: true, length: { maximum: 255 }
   validates :text2, presence: true, length: { maximum: 255 }
   
-  def image
-    flickr.photos.search(tags: text1, sort: "relevance", per_page: 1).first
+  def photo
+    photo = flickr.photos.search(text: text1, sort: "relevance", per_page: 1).first
+    getFlickrImageURL(photo, "q")
+  end
+  
+  def getFlickrImageURL(photo, size) 
+    # photoオブジェクトから画像のURLを作成して返す
+    url = "https://farm" + photo.farm.to_s + ".staticflickr.com/" + photo.server.to_s + "/" + photo.id.to_s + "_" + photo.secret.to_s;
+    if (size)   # サイズ指定ありの場合
+      url += "_" + size.to_s;
+    end
+    url += ".jpg";
+    return url;
   end
 end
